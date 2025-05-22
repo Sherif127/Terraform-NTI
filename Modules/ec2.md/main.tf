346a4f13f3cd6f37feccdd05831e630a8bff43f7
Sherif127 <Sherifshabanpp00@gmail.com>
2025-05-22 12:57:02 +0300

    Update main.tf

--- Modules/ec2.md/main.tf
+++ Modules/ec2.md/main.tf
@@ -36,21 +36,6 @@ resource "aws_instance" "my_instance" {
   }
 }
 
-# resource "null_resource" "remote_exec_enabled" {
-#   provisioner "remote-exec" {
-#     inline = var.remote_exec
-#   }
-
-#   connection {
-#     type        = "ssh"
-#     host        = aws_instance.my_instance.public_ip
-#     user        = "ec2-user"
-#      private_key = file(var.private_key)
-#   }
-#    depends_on = [aws_instance.my_instance]
-# }
-
-
 resource "aws_lb_target_group_attachment" "ec2_target" {
 
   target_group_arn = var.target_group_arn
